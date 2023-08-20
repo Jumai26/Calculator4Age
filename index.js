d2b7973239e93c970525dd4bced96275cccadacd
Gloria Williams <willyabiodun@gmail.com>
2023-08-20 18:22:52 +0100

    calculating difference

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
+
 let day = document.getElementById('day');
 let month = document.getElementById('month');
 let year = document.getElementById('year');
-let btn = document.getElementById('btn');
+
+
 let currentDate = new Date();
-console.log(currentDate);
-// let currentDay = currentDate.getDate();
-// let currentMonth = 1 + currentDate.getMonth();
-// let currentYear = currentDate.getFullYear();
-// let inputedDate = ` ${day}/${month}/${year} `;
+let currentDay = currentDate.getDate();
+let currentMonth = 1 + currentDate.getMonth();
+let currentYear = currentDate.getFullYear();
+let eachMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+let d = currentDay - day;
+let m = currentMonth - month;
+let y = currentYear - year;
+let btn = document.getElementById('btn');
 
 
 function checkAge() {
@@ -19,31 +25,31 @@ if (day.value < 01) {
     alert('month is incorrect');
 }else if (month.value > 12) {
     alert('month is incorrect');
-}else if (year.value > 2023) {
+}else (year.value > 2023); {
     alert('year is incorrect');
-} else {
-    let inputedDate = day.value + "/ " + month.value + "/ " + year.value ; 
-    console.log(inputedDate);
-}
 }
 
-btn.addEventListener('click', checkAge());
+if (day > currentDay){
+    currentDay = currentDay + eachMonth[currentMonth - 1];
+    currentMonth = currentMonth - 1;
+}
+
+if(month > currentMonth){
+    currentMonth = currentMonth + 12;
+    currentYear = currentYear - 1;
+} 
+
+let birthCount = d + " / " + m + " / " + y;
+  console.log(birthCount);
+}
+
+btn.addEventListener('click', checkAge);
 
 
 
 
 
 
-// let realAge = currentDay.getTime() - inputedDate.getTime();
-// let realAgeDiff = (realAge / 31536000000).toFixed(0);
-
-
-// btn.addEventListener('click', validation);
-
-// function validation() {
-//  let realAge = currentDate - inputedDate;
-//  console.log(realAge);
-// }
 
 
 
