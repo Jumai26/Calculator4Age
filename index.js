c0076ccb36ea1092d0220ae6d3d14bcf752d0e2f
Gloria Williams <willyabiodun@gmail.com>
2023-08-21 00:06:09 +0100

    image as button done

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-
-let day = document.getElementById('day');
-let month = document.getElementById('month');
-let year = document.getElementById('year');
-
+let day = document.getElementById("day");
+let month = document.getElementById("month");
+let year = document.getElementById("year");
 
 let currentDate = new Date();
 let currentDay = currentDate.getDate();
@@ -13,46 +11,44 @@ let eachMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 let d = currentDay - day;
 let m = currentMonth - month;
 let y = currentYear - year;
-let btn = document.getElementById('btn');
-
+let btn = document.getElementById("btn");
 
 function checkAge() {
-if (day.value < 01) {
-    alert('Day is incorrect');
-} else if (day.value > 31) {
-    alert('day is incorrect');
-} else if (month.value < 1) {
-    alert('month is incorrect');
-}else if (month.value > 12) {
-    alert('month is incorrect');
-}else (year.value > 2023); {
-    alert('year is incorrect');
-}
+  if (day.value < 01) {
+    alert("Day is incorrect");
+  } else
 
-if (day > currentDay){
-    currentDay = currentDay + eachMonth[currentMonth - 1];
-    currentMonth = currentMonth - 1;
-}
+  if (day.value > 31) {
+    alert("day is incorrect");
+  } else
 
-if(month > currentMonth){
-    currentMonth = currentMonth + 12;
-    currentYear = currentYear - 1;
-} 
+  if (month.value < 1) {
+    alert("month is incorrect");
+  } else
 
-let birthCount = d + " / " + m + " / " + y;
-  console.log(birthCount);
-}
+  if (month.value > 12) {
+    alert("month is incorrect");
+  } else
 
-btn.addEventListener('click', checkAge);
+  if (year.value > 2023);
+  {
+    alert("year is incorrect");
+  } 
 
+  if (day > currentDay) {
+    let currentDay = currentDay + eachMonth[currentMonth - 1];
+    let currentMonth = currentMonth - 1;
+  } 
+  
+  if (month > currentMonth) {
+    let currentMonth = currentMonth + 12;
+    let currentYear = currentYear - 1;
+  } 
 
-
-
-
-
-
-
-
-
+    let birthCount = d + " / " + m + " / " + y;
+    console.log(birthCount);
+  }
+  
 
 
+btn.addEventListener("click", checkAge);
